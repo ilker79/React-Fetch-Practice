eb7b4c307e340bad9aea8612a7a4911a5f12ec1f
GeorgeC <collingridgegeorge@gmail.com>
2021-02-19 09:58:00 +0000

    fetch

--- fetchapp/src/App.js
+++ fetchapp/src/App.js
@@ -1,23 +1,25 @@
+import React, { useState, useReducer, useEffect } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState();
+
+useEffect(() => {
+  getPokemon();
+}, [])
+
+async function getPokemon() {
+  let res = await fetch ("https://pokeapi.co/api/v2/pokemon/1/");
+  let data = await res.json();
+  console.log(data);
+  setPokemon(data.sprites.front_default);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src = {pokemon}/>
     </div>
   );
 }
--- fetchapp/src/Plan.md
+++ fetchapp/src/Plan.md
@@ -1,1 +1,30 @@
-#Plan
+# Disney Idiation
+
+### Dreamer
+
+- Type word in text box, finds joke to do with key word
+- Have random pokemon telling the jokes
+- Find jokes about pokemon
+- More than one pokemon have a 'joke off'
+- 2 trainers throw pkoeball and pokemon come out of the balls to joke battle
+
+### Realist
+
+- Pokemon telling random jokes
+- More than 2 pokemon having joke off
+
+### Critic
+
+- Risk running out of ****time****, wittle down to 1 pokemon telling jokes
+
+### End Posibilities
+
+- Text box
+- 2 trainers
+- Find jokes about pokemon
+
+# Plan
+
+## Api
+
+Pokemon, dad jokes
